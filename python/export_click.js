55bbc4e9dc7aace04c4dc217f3760d413b37a97f
yuxiqian <akaza_akari@sjtu.edu.cn>
2018-09-07 17:28:07 +0800

    初步实现抓取指定数据为指定格式。

---
+++ python/export_click.js
@@ -0,0 +1,34 @@
+// 页面内嵌的 js 脚本
+
+// 当导出格式下拉框被更改时会执行这段代码
+
+document.getElementById('ReportViewerControl_ctl01_ctl05_ctl01')
+    .Controller.SetViewerLinkActive(
+        document.getElementById('ReportViewerControl_ctl01_ctl05_ctl00')
+            .selectedIndex != 0);
+
+// 当点击导出按钮时会执行这段代码
+
+var formatDropDown =
+    document.getElementById('ReportViewerControl_ctl01_ctl05_ctl00');
+if (formatDropDown.selectedIndex == 0) return false;
+window.open(document.getElementById('ReportViewerControl')
+                    .ClientController.m_exportUrlBase +
+                encodeURIComponent(formatDropDown.value),
+            '_blank')
+
+// 这里的 m_exportUrlBase 猜测是
+// "\/ReportServer\/Reserved.ReportViewerWebControl.axd?ExecutionID=ohrpuh553q1eoc45fl1bdrm3&ControlID=e7215cc0600146c8a62cb4aae1b46564&Culture=2052&UICulture=4&ReportStack=1&OpType=Export&FileName=LessonArrangeForOthers&ContentDisposition=OnlyHtmlInline&Format="
+
+// encodeURIComponent encode 的就是 formatDropDown.value, 即
+// "XML"、"CSV"、"TIFF"、"MHTML" 和 "EXCEL"。
+
+formatDropDown.selectedIndex = 0;
+document.getElementById('ReportViewerControl_ctl01_ctl05_ctl01')
+    .Controller.SetViewerLinkActive(
+        document.getElementById('ReportViewerControl_ctl01_ctl05_ctl00')
+            .selectedIndex != 0);
+return false;
+
+// formatDropDown.value 可选值包括
+// "XML"、"CSV"、"TIFF"、"MHTML" 和 "EXCEL"。
