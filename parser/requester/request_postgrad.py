70e64573eab4de46880874a7e0d9a2b63953a78b
yuetsin <akaza_akari@sjtu.edu.cn>
2019-06-25 10:51:17 +0800

    [fix] summer semester starts at week #19

--- parser/requester/request_postgrad.py
+++ parser/requester/request_postgrad.py
@@ -138,10 +138,10 @@ def query_postgrad_data(start_year, term):
                         part['end_week'] = int(arr_info[1])
                     else:
                         if term == 3:
-                            part['start_week'] = int(arr_info[0]) + 19
-                            part['end_week'] = int(arr_info[1]) + 19
+                            part['start_week'] = int(arr_info[0]) + 18
+                            part['end_week'] = int(arr_info[1]) + 18
                             print("Relocate week to %d - %d" %
-                                  (int(arr_info[0]) + 19, int(arr_info[1]) + 19))
+                                  (int(arr_info[0]) + 18, int(arr_info[1]) + 18))
                         else:
                             part['start_week'] = int(arr_info[0])
                             part['end_week'] = int(arr_info[1])
